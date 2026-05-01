5480bb5804da5bcbaa2fb0b8fefef6199ea46c51
Yani <yanis051989@gmail.com>
2026-04-30 20:13:33 -0500

    add and delete done

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 var taskInput = document.getElementById("taskInput");
 var addTask = document.getElementById("addTask");
 var taskList = document.getElementById("taskList");
+
+addTask.addEventListener("click", addNewTask);
+
+ function addNewTask() {
+    
+    //get the user input
+
+    var taskName = taskInput.value.trim();
+    if(taskName === "") 
+        return;
+
+    //create the element that represents the task
+     var li =document.createElement("li");
+     li.innerHTML = '<input type="checkbox"> ' + 
+     '<span class="task">' +
+     taskName + ' </span>' + 
+     ' <button class="delete">Delete</button>';
+    
+    
+    var deleteButton = li.querySelector(".delete");
+    deleteButton.addEventListener("click", () =>{
+        li.remove();
+    });
+    
+    // render the task in the task list
+
+    taskList.appendChild(li)
+    //reset the value i the input
+
+        taskInput.value = "";
+ }
